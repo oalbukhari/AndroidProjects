f3d4ba370a4208b7ed56178f8f770aa13e3499b0
Omar Al Bukhari <albukhariomar4@gmail.com>
2017-04-28 01:04:45 +0200

    StartProjects
    3 projects to learn new tchnologies...(databinding, realm, rxJava2)

---
+++ NewAndroid/RxProject/app/src/main/java/com/omar_al_bukhari/rxproject/MainActivity.java
@@ -0,0 +1,13 @@
+package com.omar_al_bukhari.rxproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
